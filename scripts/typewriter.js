b857051c095d456b164532c71dcbc8605a91e095
Will Crawford <will@Wills-MacBook-Pro.local>
2020-10-08 15:19:01 -0400

    add typewriter and animations

---
+++ scripts/typewriter.js
@@ -0,0 +1,27 @@
+const typeWriter = document.getElementById('companionMethods')
+typeWriter.addEventListener('load', newType())
+
+function newType(){
+new TypeIt("#companionMethods", {
+    speed: 50,
+    waitUntilVisible: true
+  })
+    .type("Nvver", {delay: 300})
+    .move(-3)
+    .delete(1)
+    .type('e')
+    .move('END')
+    .type(' let yees')
+    .pause(300)
+    .delete(2)
+    .type('sterday use up to muc')
+    .move(-4)
+    .type('o')
+    .move('END')
+    .type('h of today.')
+    .pause(500)
+    .break({delay: 500})
+    .break({delay: 500})
+    .type('<em>- Will Rogers</em>')
+    .go();
+}
